cc89008c6804e852db3d62e4ed0b10a42fa309bb
bala-amypo <bala@amypo.in>
2025-12-18 09:17:42 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -3,6 +3,11 @@
  import jakarta.persistence;
 
  public class Gargage{
+    @Id
+    @Table()
     private Long id;
-    private
+    private String gargageName;
+    private String address;
+    private String contactNumber;
+    private Boolean active;
  }
