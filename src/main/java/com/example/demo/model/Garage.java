f58c31a23fbfb698c12dcecb82b2df45272b79fa
bala-amypo <bala@amypo.in>
2025-12-18 15:35:19 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -1,19 +1,27 @@
- package com.example.demo.model;
+package com.example.demo.model;
 
-import jakarta.persistence;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @Table(
-    name="garages",
+    name = "garages",
     uniqueConstraints = @UniqueConstraint(columnNames = "garageName")
 )
- public class Garage{
+public class Garage {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private String garageName;
+
     private String address;
     private String contactNumber;
 
@@ -70,5 +78,4 @@ import jakarta.persistence;
     public void setActive(Boolean active) {
         this.active = active;
     }
-    
- }
+}
