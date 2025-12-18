065c45348ea24e665ce1b8f696fbf86dd036b3f0
bala-amypo <bala@amypo.in>
2025-12-18 09:27:44 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -1,16 +1,74 @@
  package com.example.demo.model;
 
- import jakarta.persistence;
+import jakarta.persistence;
 
 @Entity
 @Table(
-    name="garage"
+    name="garages",
+    uniqueConstraints = @UniqueConstraint(columnNames = "garageName")
 )
- public class Gargage{
+ public class Garage{
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String gargageName;
+
+    @Column(nullable = false)
+    private String garageName;
     private String address;
     private String contactNumber;
+
+    @Column(nullable = false)
     private Boolean active;
+
+    public Garage(Long id, String garageName, String address, String contactNumber, Boolean active) {
+        this.id = id;
+        this.garageName = garageName;
+        this.address = address;
+        this.contactNumber = contactNumber;
+        this.active = active;
+    }
+
+    public Garage() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGarageName() {
+        return garageName;
+    }
+
+    public void setGarageName(String garageName) {
+        this.garageName = garageName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    
  }
