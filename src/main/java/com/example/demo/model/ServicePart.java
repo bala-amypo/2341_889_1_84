5c3ca1560fd4ddfd04d0c3b01357123055b5fd56
bala-amypo <bala@amypo.in>
2025-12-18 09:37:43 +0000

    User commit

--- src/main/java/com/example/demo/model/ServicePart.java
+++ src/main/java/com/example/demo/model/ServicePart.java
@@ -12,5 +12,75 @@
 
     @manyToOne(optional = false)
     @JoinColumn(name = "service_entry_id")
-    private ServiceEntry serviceEntry
+    private ServiceEntry serviceEntry;
+
+    @Column(nullable = false)
+    private String partName;
+    private String partNumbr;
+    @Column(nullable = false)
+    private BigDecimal cost;
+    @Column(nullable = false)
+    private int quantity;
+    
+    public ServicePart(Long id, ServiceEntry serviceEntry, String partName, String partNumbr, BigDecimal cost,
+         int quantity) {
+      this.id = id;
+      this.serviceEntry = serviceEntry;
+      this.partName = partName;
+      this.partNumbr = partNumbr;
+      this.cost = cost;
+      this.quantity = quantity;
+    }
+
+    public ServicePart() {
+    }
+
+    public Long getId() {
+       return id;
+    }
+
+    public void setId(Long id) {
+       this.id = id;
+    }
+
+    public ServiceEntry getServiceEntry() {
+       return serviceEntry;
+    }
+
+    public void setServiceEntry(ServiceEntry serviceEntry) {
+       this.serviceEntry = serviceEntry;
+    }
+
+    public String getPartName() {
+       return partName;
+    }
+
+    public void setPartName(String partName) {
+       this.partName = partName;
+    }
+
+    public String getPartNumbr() {
+       return partNumbr;
+    }
+
+    public void setPartNumbr(String partNumbr) {
+       this.partNumbr = partNumbr;
+    }
+
+    public BigDecimal getCost() {
+       return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+       this.cost = cost;
+    }
+
+    public int getQuantity() {
+       return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+       this.quantity = quantity;
+    }
+
  }
