e11758236cc2387708d4731aea2da5e5fdf90eea
bala-amypo <bala@amypo.in>
2025-12-18 09:07:42 +0000

    User commit

--- src/main/java/com/example/demo/model/ServiceEntry.java
+++ src/main/java/com/example/demo/model/ServiceEntry.java
@@ -7,12 +7,27 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "service_entries")
 public class ServiceEntry{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @manytoOne(optional = false)
+    @joinColumn(name = "vehicle_id")
     private Vehicle vehicle;
+    @manytoOne(optional = false)
+    @joinColumn(name = "garge_id")
     private Gargage gargage;
+    @Column(nullable=false)
     private String serviceType;
+    @Column(nullable=false)
     private LocalDate serviceDate;
+    @Column(nullable=false)
     private int odometerReading;
     private String description;
-    private LocalDateTime recorded
+    @Column(nullable=false, updatable = false)
+    private LocalDateTime recordedAt;
+
+    @Prepersist
+    protected void onCreate(){
+        this.recordedAt = Local
+    }
 }
