0d0aee386569d4431120f27cf7e166df9c8db6ff
bala-amypo <bala@amypo.in>
2025-12-18 08:52:45 +0000

    User commit

--- src/main/java/com/example/demo/model/ServiceEntry.java
+++ src/main/java/com/example/demo/model/ServiceEntry.java
@@ -1,1 +1,12 @@
-package  com.example
+package  com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "service_entries")
+public class ServiceEntry{
+    private Long id;
+    private 
+}
