b1c7d8c45ff707d11819a7a5ad0c0908630e6194
bala-amypo <bala@amypo.in>
2025-12-18 08:42:43 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,6 @@
- package com.example.demo.model;
+package com.example.demo.model;
+import jakarta.persistence.*;
+
 
  public class User{
     @Id
@@ -7,4 +9,39 @@
     private String email;
     private String password;
     private String role;
+    public User(Long id, String email, String password, String role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
  }
