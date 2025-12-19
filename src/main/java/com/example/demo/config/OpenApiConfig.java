951b182e4b930ead732b0f39796178cb32f453fb
bala-amypo <bala@amypo.in>
2025-12-19 07:57:39 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -19,7 +19,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 // Server configuration (kept exactly as yours)
                 .servers(List.of(
-                        new Server().url("https://9021.pro604cr.amypo.ai/")
+                        new Server().url("https://9343.pro604cr.amypo.ai/")
                 ))
 
                 // JWT Security Scheme
