a63b62dc058ed6fdf64195e59235301a2ced7e53
bala-amypo <bala@amypo.in>
2025-12-19 04:08:17 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -22,8 +22,6 @@ public class Garage {
     @Column(nullable = false)
     private Boolean active = true;
 
-    // ===== Getters & Setters =====
-
     public Long getId() {
         return id;
     }
