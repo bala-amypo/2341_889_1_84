9c20c7e3ca9026fd15a45d98e3a7e4a335aa8b13
bala-amypo <bala@amypo.in>
2025-12-19 06:24:11 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ServicePartServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ServicePartServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.EntityNotFoundException;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.ServicePart;
 import com.example.demo.model.ServiceEntry;
 import com.example.demo.repository.ServicePartRepository;
