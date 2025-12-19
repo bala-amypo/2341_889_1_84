336b2e1e1a6895f486c954dc8cefea9748d8db39
bala-amypo <bala@amypo.in>
2025-12-19 06:12:11 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ServicePartServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ServicePartServiceImpl.java
@@ -1,38 +1,26 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.EntityNotFoundException;
 import com.example.demo.model.ServicePart;
 import com.example.demo.repository.ServicePartRepository;
 import com.example.demo.service.ServicePartService;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class ServicePartServiceImpl implements ServicePartService {
 
-    private final ServicePartRepository repository;
+    private final ServicePartRepository servicePartRepository;
 
-    public ServicePartServiceImpl(ServicePartRepository repository) {
-        this.repository = repository;
+    public ServicePartServiceImpl(ServicePartRepository servicePartRepository) {
+        this.servicePartRepository = servicePartRepository;
     }
 
     @Override
     public ServicePart createPart(ServicePart part) {
+
         if (part.getQuantity() <= 0) {
-            throw new IllegalArgumentException("Quantity");
+            throw new IllegalArgumentException("Quantity must be greater than zero");
         }
-        return repository.save(part);
-    }
 
-    @Override
-    public ServicePart getPartById(Long id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Part not found"));
-    }
-
-    @Override
-    public List<ServicePart> getPartsForEntry(Long entryId) {
-        return repository.findByServiceEntryId(entryId);
+        return servicePartRepository.save(part);
     }
 }
