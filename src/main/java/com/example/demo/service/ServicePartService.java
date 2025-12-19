bdc63fe2e023217bbbe1547940d2a37fe74790f0
bala-amypo <bala@amypo.in>
2025-12-19 06:33:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ServicePartService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.model.ServicePart;
+import java.util.List;
+
+public interface ServicePartService {
+
+    ServicePart createPart(Long entryId, ServicePart part);
+
+    ServicePart getPartById(Long id);
+
+    List<ServicePart> getPartsForEntry(Long entryId);
+}
