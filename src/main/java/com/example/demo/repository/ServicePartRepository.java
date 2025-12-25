f0810415e8caec8d37e1c2a20258c6c6f0a9d250
bala-amypo <bala@amypo.in>
2025-12-25 17:17:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/ServicePartRepository.java
+++ src/main/java/com/example/demo/repository/ServicePartRepository.java
@@ -3,5 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.model.ServicePart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServicePartRepository extends JpaRepository<ServicePart, Long> {
+import java.util.List;
+
+public interface ServicePartRepository
+        extends JpaRepository<ServicePart, Long> {
+
+    List<ServicePart> findByServiceEntryId(Long serviceEntryId);
 }
