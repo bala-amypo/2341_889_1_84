8bbba5461d5fc1edf15b827b6a98e15d73b0216a
bala-amypo <bala@amypo.in>
2025-12-28 15:02:38 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,5 +1,4 @@
 package com.example.demo.security;
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -7,14 +6,11 @@ import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
-
 @Component
 public class JwtTokenProvider {
-
     private static final String SECRET = "mysecretkeymysecretkeymysecretkeymysecretkey";
     private final Key key = Keys.hmacShaKeyFor(Base64.getEncoder().encode(SECRET.getBytes()));
     private final long expiration = 86400000;
-
     public String generateToken(String email, String role, Long userId) {
         return Jwts.builder()
                 .setSubject(email)
@@ -25,7 +21,6 @@ public class JwtTokenProvider {
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
-
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
@@ -34,7 +29,6 @@ public class JwtTokenProvider {
             return false;
         }
     }
-
     public String getEmailFromToken(String token) {
         return Jwts.parserBuilder().setSigningKey(key).build()
                 .parseClaimsJws(token)
