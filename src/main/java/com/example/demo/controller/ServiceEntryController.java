3e745f4041c9554f001639e539b99f5c9ef84396
bala-amypo <bala@amypo.in>
2025-12-28 14:38:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/ServiceEntryController.java
+++ src/main/java/com/example/demo/controller/ServiceEntryController.java
@@ -18,22 +18,18 @@ public class ServiceEntryController {
     public ServiceEntryController(ServiceEntryService serviceEntryService) {
         this.serviceEntryService = serviceEntryService;
     }
-
     @PostMapping
     public ResponseEntity<ServiceEntry> createServiceEntry(@RequestBody ServiceEntry entry) {
         return ResponseEntity.ok(serviceEntryService.createServiceEntry(entry));
     }
-
     @GetMapping("/{id}")
     public ResponseEntity<ServiceEntry> getEntryById(@PathVariable Long id) {
         return ResponseEntity.ok(serviceEntryService.getServiceEntryById(id));
     }
-
     @GetMapping("/vehicle/{vehicleId}")
     public ResponseEntity<List<ServiceEntry>> getEntriesForVehicle(@PathVariable Long vehicleId) {
         return ResponseEntity.ok(serviceEntryService.getEntriesForVehicle(vehicleId));
     }
-
     @GetMapping("/garage/{garageId}")
     public ResponseEntity<List<ServiceEntry>> getEntriesByGarage(@PathVariable Long garageId) {
         return ResponseEntity.ok(serviceEntryService.getEntriesByGarage(garageId));
